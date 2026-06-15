f9d1d75181af76df5ec275cf431efb2b35fe0a9e
stuchnk <iss04122007@gmail.com>
2026-06-15 09:18:25 +0300

    Удалить папку e-journal (перенесена в Zhournal)

--- e-journal/src/components/ui/Loader.tsx
+++
@@ -1,8 +0,0 @@
-export function Loader({ text = "Загрузка..." }: { text?: string }) {
-  return (
-    <div className="loader">
-      <div className="spinner" />
-      <div>{text}</div>
-    </div>
-  );
-}
